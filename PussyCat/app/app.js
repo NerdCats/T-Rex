7b40e19b17f333ef2da5e2d00d92a90ea91e4956
Swagata Prateek <s.prateek@hotmail.com>
2016-03-09 19:05:39 +0600

    Added the interceptor, need to make it work though

--- PussyCat/app/app.js
+++ PussyCat/app/app.js
@@ -2,55 +2,60 @@
 
 
 var app = angular.module('app', [
-	'ngMaterial',
-	'ngMessages',
-	'ngRoute',
-	'LocalStorageModule'
+  'ngMaterial',
+  'ngMessages',
+  'ngRoute',
+  'LocalStorageModule'
 ]);
 
 app.config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-			when('/',{
-				templateUrl: 'app/views/dashboard.html',
-		    controller: 'dashBoardController',
-		    controllerAs: 'dashboard'
-			}).
-			when('/login', {
-				templateUrl: 'app/views/login.html',
-				controller: 'authController',
-				controllerAs: 'auth'
-			}).
-			when('/details/:id',{
-				templateUrl: 'app/views/detailsJob.html',
-				controller: 'detailsController',
-				controllerAs: 'details'
-			}).
-			when('/asset', {
-				templateUrl: 'app/views/assets.html',
-				controller: 'assetController',
-			}).
-			when('/asset/create',{
-				templateUrl: 'app/views/asset/create.html',
-				controller: 'createAssetController',
-				controllerAs: 'createAsset'
-			})
-			$routeProvider.otherwise({ redirectTo: "/"});
-	}
+  function($routeProvider) {
+    $routeProvider.
+    when('/', {
+      templateUrl: 'app/views/dashboard.html',
+      controller: 'dashBoardController',
+      controllerAs: 'dashboard'
+    }).
+    when('/login', {
+      templateUrl: 'app/views/login.html',
+      controller: 'authController',
+      controllerAs: 'auth'
+    }).
+    when('/details/:id', {
+      templateUrl: 'app/views/detailsJob.html',
+      controller: 'detailsController',
+      controllerAs: 'details'
+    }).
+    when('/asset', {
+      templateUrl: 'app/views/assets.html',
+      controller: 'assetController',
+    }).
+    when('/asset/create', {
+      templateUrl: 'app/views/asset/create.html',
+      controller: 'createAssetController',
+      controllerAs: 'createAsset'
+    })
+    $routeProvider.otherwise({
+      redirectTo: "/"
+    });
+  }
 ]);
 
 app.config(function($mdThemingProvider) {
-	// Configure a dark theme with primary foreground yellow
-	$mdThemingProvider.theme('docs-dark', 'default')
-	.primaryPalette('grey')
-	.dark();
+  // Configure a dark theme with primary foreground yellow
+  $mdThemingProvider.theme('docs-dark', 'default')
+    .primaryPalette('grey')
+    .dark();
 });
 
 angular.module('app').constant('ngAuthSettings', {
-	apiServiceBaseUri: "http://localhost:23873/",
-	clientId:'GoFetchDevWebApp'
+  apiServiceBaseUri: "http://localhost:23873/",
+  clientId: 'GoFetchDevWebApp'
 });
 
+app.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.interceptors.push('authInterceptorService');
+}]);
 
 
 // app.config(function($locationProvider) {
