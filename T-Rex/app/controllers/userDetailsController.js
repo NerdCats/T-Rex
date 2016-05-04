76de1ebee804684c22e2719947a07eb749058e48
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2016-05-04 13:05:28 +0600

    user profile details page added, now showing basic information

--- T-Rex/app/controllers/userDetailsController.js
+++ T-Rex/app/controllers/userDetailsController.js
@@ -1,12 +1,26 @@
 app.controller('userDetailsController', ['$scope', '$routeParams', 'userService', 'host', 'restCall', function($scope, $routeParams, userService, host, restCall){
+	
 	var vm = this;
 	var id = $routeParams.id;
 	vm.User = {};
-	
+	vm.isAsset = false;
+	vm.isEnterprise = false;
+	vm.isUser = false;
+
+
 	var userUrl = host + "api/account/profile/" + id;
+
 	function userFound(response) {
 		vm.User = response.data;
-		console.log(vm.User);
+		if (vm.User.Type == "USER") 
+			vm.isUser = true;
+		else if (vm.User.Type == "BIKE_MESSENGER" || vm.User.Type == "CNG_DRIVER")
+			vm.isAsset = true;
+		else if (vm.User.Type == "ENTERPRISE") 
+			vm.isEnterprise = true;
+
+		console.log(vm.User.Type);
+		console.log(vm.isUser);
 	}
 	function userNotFound(error) {
 		console.log(error)
