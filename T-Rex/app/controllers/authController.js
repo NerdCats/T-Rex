7d3d7fe0f8d07e378056102e300b1ded04f0775d
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2016-05-05 14:33:26 +0600

    loading bar added on login button

--- T-Rex/app/controllers/authController.js
+++ T-Rex/app/controllers/authController.js
@@ -1,39 +1,40 @@
 'use strict';
 
-angular
-  .module('app')
-  .controller('authController', authController);
+app.controller('authController', authController);
 
 authController.$inject = ['$scope', 'authService', '$location', '$window'];
 
 function authController($scope, authService, $location, $window) {
-  var vm = this;
-  vm.loginData = {};
-  vm.loginData.username = "";
-  vm.loginData.password = "";
-  vm.loginData.email = "";
+    var vm = this;
+    vm.loginData = {};
+    vm.loginData.username = "";
+    vm.loginData.password = "";
+    vm.loginData.email = "";
+    vm.logginOnProcess = false;
 
-  vm.loginData.useRefreshTokens = false;
-  vm.login = login;
+    vm.loginData.useRefreshTokens = false;
+    vm.login = login;
 
-  activate();
+    activate();
 
-  function activate() {
-	if ($window.location.hash == '#/login'){
- 			vm.sidebarVisible = false;
- 			vm.shouldShowMenuAndFooter = false;			
- 		}
-  }
+    function activate() {
+        if ($window.location.hash == '#/login'){
+            vm.sidebarVisible = false;
+            vm.shouldShowMenuAndFooter = false;			
+        }
+    }
 
-  function login() {
-	authService.login(vm.loginData).then(function(response) {
-		console.log("log in success");
-		console.log(response);
-		$window.location.reload();
-	  },
-	  function(err) {
-		console.log(err);
-		$scope.message = err.error_description;
-	  });
-  }
+    function login() {
+        vm.logginOnProcess = true;
+        authService.login(vm.loginData).then(function(response) {
+            console.log("log in success");
+            console.log(response);
+            $window.location.reload();
+        },
+        function(err) {
+            console.log(err);
+            vm.logginOnProcess = false;
+            $scope.message = err.error_description;
+        });
+    }
 }
