fc431527b25ec61e2485ab1d8dde66a255ad33f7
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2016-08-24 14:56:06 +0600

    user registration form html added

--- T-Rex/app/controllers/usercreateC.js
+++ T-Rex/app/controllers/usercreateC.js
@@ -3,46 +3,33 @@ app.controller('usercreateC', ['$scope', 'userService', usercreateC]);
 
 function usercreateC($scope,userService) {
 
-  var vm = this;
+  var vm = $scope;
 
   vm.gender = ["MALE", "FEMALE"];
-  vm.type = ["USER", "CNG_DRIVER", "BIKE_MESSENGER", "ENTERPRISE"];
+  vm.userTypes = ["USER", "CNG_DRIVER", "BIKE_MESSENGER", "ENTERPRISE"];
 
-    vm.isAsset = false;
-    vm.isEnterpriseUser = false;
+	vm.userType = "USER";
+	vm.user = {
+		UserName : "",
+		Password : "",
+		ConfirmPassword : "",
+		Email : "",
+		PhoneNumber : "",
+		PicUri : "",
+		Type : "",
+		FirstName : "",
+		LastName : "",
+		Age : "",
+		Gender : "",
+		Address : "",      
+		NationalId : "",
+		DrivingLicenceId : "",
+		ContactPersonName : "",
+		Website : ""
+	};
 
-    vm.asset = {
-      UserName : "",
-      Password : "",
-      ConfirmPassword : "",
-      Email : "",
-      PhoneNumber : "",
-        PicUri : "",
-      Type : "",
-        FirstName : "",
-        LastName : "",
-        Age : "",
-        Gender : "",
-        Address : "",      
-      NationalId : "",
-      DrivingLicenceId : "",
-        ContactPersonName : "",
-        Website : ""
-    };
-
-    vm.UserTypeChanged = function (type) {
-        if (type == "BIKE_MESSENGER" || type == "CNG_DRIVER") {
-            vm.isAsset = true;
-            vm.isEnterpriseUser = false;
-        } else if (type == "ENTERPRISE") {
-            vm.isAsset = false;
-            vm.isEnterpriseUser = true;
-        }
-        console.log(type)
-    }
-    
-    vm.RegisterNewAsset = function () {
-      userService.registerNewUser(vm.asset);
-    } 
-    
+	vm.RegisterNewAsset = function () {
+		userService.registerNewUser(vm.user);
+	} 
+	
 }
