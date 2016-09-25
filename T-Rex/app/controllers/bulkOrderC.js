8288d5c0a2812d03e2f7dcd43a065c34903d378d
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2016-09-25 18:11:01 +0600

    table and csv file is loaded, need to create order now

--- T-Rex/app/controllers/bulkOrderC.js
+++ T-Rex/app/controllers/bulkOrderC.js
@@ -1,13 +1,25 @@
+// FIXME: someday, you should use http://oss.sheetjs.com/js-xlsx/
+
 app.controller('bulkOrderC', ['$scope', bulkOrderC]);
 
 function bulkOrderC ($scope) {
 	
 	var vm = $scope;
+	
+	document.getElementById('csv').addEventListener('change', handleFile, false);
+	function handleFile (e) {		
+		var files = e.target.files;
+		var f = files[0];
+		console.log(f)
 
-	vm.CSV;
-
-	vm.uploadFile = function () {		
-		console.log(vm.CSV);
+		 var r = new FileReader();
+          r.onload = function(e) {
+              var contents = e.target.result;              
+              vm.CSV = vm.readCSV(contents);
+              vm.$apply();
+              console.log(vm.CSV);
+          };
+          r.readAsText(files[0]);
 	}
 
 	vm.readCSV = function (input) {
@@ -26,7 +38,11 @@ function bulkOrderC ($scope) {
 		        totalweight: o[7],
 		        totalprice: o[8],
 		        servicecharge: o[9],
-		        notetodeliveryman : o[10]
+		        notetodeliveryman : o[10],
+		        editMode: false,
+		        createOrder: function () {
+		        	
+		        }
 		    });
 	    });
 	    return obj;
