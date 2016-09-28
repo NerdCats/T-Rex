23351f4ed71bdb713e987a5a10a02e71f6cc8df7
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2016-09-28 14:18:55 +0600

    GFETCH-138 added first gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var ngmin = require('gulp-ngmin');
 
 gulp.task('prod', function () {
-	return gulp.src('app/**/**/*.js')
-			.pipe(gulp.dest('dest'));
+	return gulp.src('app/**/**/*.js')			
+			.pipe(gulp.dest('dist'));
 })
