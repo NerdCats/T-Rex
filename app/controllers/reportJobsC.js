4c4cd65398d69d03977812cee41ee2b0dce835f4
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2016-10-09 10:39:01 +0600

    added seller info buyer info on job details page

--- app/controllers/reportJobsC.js
+++ app/controllers/reportJobsC.js
@@ -12,13 +12,6 @@ function reportJobsC($scope, $routeParams, $http, reportJobsService, dashboardFa
 	if ($routeParams.usertype) vm.report.searchParam.usertype = $routeParams.usertype;
 	if ($routeParams.userid) vm.report.searchParam.userid = $routeParams.userid;
 
-	console.log("vm.report.searchParam.startdate : " + vm.report.searchParam.startdate)
-	console.log("vm.report.searchParam.enddate : " + vm.report.searchParam.enddate)
-	console.log("vm.report.searchParam.state : " + vm.report.searchParam.state)
-	console.log("vm.report.searchParam.username : " + vm.report.searchParam.username)
-	console.log("vm.report.searchParam.usertype : " + vm.report.searchParam.usertype)
-	console.log("vm.report.searchParam.userid : " + vm.report.searchParam.userid)
-
 	vm.activate = function () {
 		vm.report.loadReport();
 	}
