ddbb53533750acb61f9e993a364d614237abb345
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2016-10-13 12:27:44 +0600

    added product categories modal in Store form

--- app/controllers/storeC.js
+++ app/controllers/storeC.js
@@ -1,6 +1,6 @@
-app.controller('storeC', ['$scope', '$routeParams', storeC]);
+app.controller('storeC', ['$scope', '$routeParams', '$uibModal', storeC]);
 
-function storeC($scope, $routeParams){
+function storeC($scope, $routeParams, $uibModal){
 	var vm = $scope;
 	vm.title = "hello store!"
 	vm.enterpriseId = $routeParams.id;
@@ -19,6 +19,61 @@ function storeC($scope, $routeParams){
 			}
 
 	vm.addProductCategories = function () {
-		
+		var catagoriesModalInstance = $uibModal.open({
+			animation: $scope.animationEnabled,
+			templateUrl: 'app/views/modals/addProductCategoris.html',
+			controller: 'categoriesModalC'
+		});
+
+		catagoriesModalInstance.result.then(function (categories) {
+			vm.store.ProductCategories.push(categories);
+		}, function () {
+			console.log("discarded");
+		})
+	}	
+}
+
+
+app.controller('categoriesModalC', ['$scope', '$http', '$uibModalInstance', 'ngAuthSettings', 'filterFilter', categoriesModalC]);
+
+function categoriesModalC($scope, $http, $uibModalInstance, ngAuthSettings, filterFilter){
+	
+	var vm = $scope;
+
+	vm.params = {
+		pageSize : 50,
+		page: 0,
+		envelope: true
 	}
+
+    vm.catagories = [];
+    vm.selectedCatagory = {};
+
+    vm.loadCatagories = function () {
+    	vm.catagoriesUrl = ngAuthSettings.apiServiceBaseUri + 
+							"api/ProductCategory/odata?pageSize=" + vm.params.pageSize
+														+"&page="+ vm.params.page 
+														+"&envelope="+ vm.params.envelope;
+		$http({
+			method: 'GET',
+			url: vm.catagoriesUrl
+		}).then(function success(response) {
+			vm.catagories = response.data;
+		}, function error(error) {
+			console.log(error);
+		});
+    }
+
+	vm.selectCatagorie = function () {
+		// body...
+	}
+
+	vm.select = function () {
+		console.log(vm.selectedCatagory);
+		$uibModalInstance.close(vm.selectedCatagory);
+	}
+
+
+
+
 }
