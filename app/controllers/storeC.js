55f481cfd4025b6bb22d0623b6be84fd9525e7a5
Muhammed Tareq Aziz <tareqaziz.aust.cse@gmail.com>
2016-10-13 15:10:08 +0600

    store form completed

--- app/controllers/storeC.js
+++ app/controllers/storeC.js
@@ -1,12 +1,18 @@
-app.controller('storeC', ['$scope', '$routeParams', '$uibModal', storeC]);
+app.controller('storeC', ['$scope', '$routeParams', '$uibModal', '$http', 'ngAuthSettings', storeC]);
 
-function storeC($scope, $routeParams, $uibModal){
+function storeC($scope, $routeParams, $uibModal, $http, ngAuthSettings){
 	var vm = $scope;
 	vm.title = "hello store!"
 	vm.enterpriseId = $routeParams.id;
+	vm.creatingStore = false;
+	vm.loadingStores = true;
+	vm.errmsg = null;
+	vm.editingMode = false;
+	vm.user = null;
 	console.log(vm.enterpriseId);
 
-	vm.store = {
+	vm.getStore = function () {
+		return {
 				  Name: null,
 				  Url: null,
 				  DisplayOrder: 0,
@@ -14,9 +20,76 @@ function storeC($scope, $routeParams, $uibModal){
 				  ProductCategories: [
 				    
 				  ],
-				  CoverPicUrl: "string",
-				  Id: "string"
+				  CoverPicUrl: null			  
 			}
+	}
+
+	vm.loadUser = function () {
+		$http({
+			method: 'GET',
+			url: ngAuthSettings.apiServiceBaseUri + "api/Account/Profile/" + vm.enterpriseId
+		}).then(function (response) {
+			vm.user = response.data;
+			console.log(response)
+		}, function (error) {
+			console.log(error);
+			vm.errmsg = error.Message;
+		})
+	}
+	vm.loadStores = function () {
+		vm.loadingStores = true;
+		$http({
+			method: 'GET',
+			url: ngAuthSettings.apiServiceBaseUri + "api/Store/odata?$filter=EnterpriseUserId eq '" + vm.enterpriseId + "'", //i dont think we will ever need pagination here			
+		}).then(function (response) {
+			vm.loadingStores = false;
+			vm.stores = response.data.data;
+			console.log(vm.stores)
+		}, function (error) {
+			vm.loadingStores = false;
+			console.log(error);
+		})
+	}
+
+	vm.stores = [];
+	vm.store = vm.getStore();
+	vm.loadUser();
+	vm.loadStores();
+
+	vm.editModeOn = function (_store) {
+		vm.store = _store;
+		vm.editingMode = true;
+	}
+
+	vm.editStore = function () {
+		$http({
+			method: 'PUT',
+			url: ngAuthSettings.apiServiceBaseUri + "api/Store",
+			data: vm.store
+		}).then(function (response) {
+			vm.store = vm.getStore();
+			vm.editModeOn = false;
+			vm.loadStores();
+		}, function (error) {
+			vm.errmsg = error.Message;
+		})
+	}
+
+	vm.clearEdit = function () {
+		vm.editingMode = false;
+		vm.store = vm.getStore();
+	}
+
+	vm.deleteStore = function (_store) {
+		$http({
+			method: 'DELETE',
+			url: ngAuthSettings.apiServiceBaseUri + "api/Store/" + _store.Id,			
+		}).then(function (response) {			
+			vm.loadStores();
+		}, function (error) {
+			vm.errmsg = error.Message;
+		})
+	}
 
 	vm.addProductCategories = function () {
 		var catagoriesModalInstance = $uibModal.open({
@@ -26,18 +99,40 @@ function storeC($scope, $routeParams, $uibModal){
 		});
 
 		catagoriesModalInstance.result.then(function (categories) {
+			console.log(categories);
 			vm.store.ProductCategories.push(categories);
 		}, function () {
 			console.log("discarded");
 		})
-	}	
+	}
+
+	vm.removeCategory = function (index) {
+		vm.store.ProductCategories.splice(index, 1);
+	}
+
+	vm.createStore = function () {
+		vm.creatingStore = true;
+		vm.errmsg = null;
+		$http({
+			method: 'POST',
+			url: ngAuthSettings.apiServiceBaseUri + "api/Store",
+			data: vm.store
+		}).then(function (response) {
+			vm.store = vm.getStore();
+			vm.creatingStore = false;
+			vm.loadStores();
+		}, function (error) {
+			vm.errmsg = error.Message;
+			vm.creatingStore = false;
+		});
+	}
 }
 
 
-app.controller('categoriesModalC', ['$scope', '$http', '$uibModalInstance', 'ngAuthSettings', 'filterFilter', categoriesModalC]);
+app.controller('categoriesModalC', ['$scope', '$http', '$uibModalInstance', 'ngAuthSettings', categoriesModalC]);
 
-function categoriesModalC($scope, $http, $uibModalInstance, ngAuthSettings, filterFilter){
-	
+function categoriesModalC($scope, $http, $uibModalInstance, ngAuthSettings){
+
 	var vm = $scope;
 
 	vm.params = {
@@ -48,31 +143,39 @@ function categoriesModalC($scope, $http, $uibModalInstance, ngAuthSettings, filt
 
     vm.catagories = [];
     vm.selectedCatagory = {};
+    vm.isLoadingCategories = true;
+    vm.searchText = "";
 
     vm.loadCatagories = function () {
     	vm.catagoriesUrl = ngAuthSettings.apiServiceBaseUri + 
 							"api/ProductCategory/odata?pageSize=" + vm.params.pageSize
-														+"&page="+ vm.params.page 
+														+"&page="+ vm.params.page
 														+"&envelope="+ vm.params.envelope;
 		$http({
 			method: 'GET',
 			url: vm.catagoriesUrl
 		}).then(function success(response) {
-			vm.catagories = response.data;
+			vm.isLoadingCategories = false;
+			vm.catagories = response.data.data;
+			console.log(vm.catagories)
 		}, function error(error) {
+			vm.isLoadingCategories = false;
 			console.log(error);
 		});
     }
 
-	vm.selectCatagorie = function () {
-		// body...
+	vm.selectCatagorie = function (cat) {
+		vm.selectedCatagory = cat;
 	}
 
-	vm.select = function () {
-		console.log(vm.selectedCatagory);
+
+
+	vm.select = function () {		
 		$uibModalInstance.close(vm.selectedCatagory);
 	}
 
+
+	vm.loadCatagories();
 
 
 
