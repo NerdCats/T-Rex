f8d545c87795eabb427dddeebe95ff1aa19d082e
Swagata Prateek <thehoneymad@hotmail.com>
2016-11-03 14:38:30 +0100

    Pointing to new development system

--- app/constants.js
+++ app/constants.js
@@ -1,6 +1,6 @@
 app.constant('ngAuthSettings', {
   // apiServiceBaseUri: "http://taskcatdev.azurewebsites.net/",
-  apiServiceBaseUri: "http://gofetch.cloudapp.net:80/",
+  apiServiceBaseUri: "http://fetchdev.gobd.co/",
   // apiServiceBaseUri: "http://localhost:23873/",
   clientId: 'GoFetchDevWebApp'
 });
