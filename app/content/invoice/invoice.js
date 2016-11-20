3674b180f3a79e6cc110b91957627fb6b1278628
tareq89 <tareqaziz.aust.cse@gmail.com>
2016-11-20 13:48:13 +0600

    GFETCH-236 : invoice module now takes a comma separated list of HRID and render them all in one single page

--- app/content/invoice/invoice.js
+++ app/content/invoice/invoice.js
@@ -4,69 +4,49 @@ app.controller('invoiceCtrl',['$scope', '$http', '$window', invoiceCtrl]);
 function invoiceCtrl($scope, $http, $window) {
     var vm = $scope;
     vm.loadingJob = true;
-    var jobId = $window.location.search.substring(1);
+    var jobId = $window.location.search.substring(1).split(",");
     vm.today = new Date();
-    vm.job = {};
+    // vm.job = {};
+    vm.jobs= [];
 
-    var prod = "http://fetchprod.gobd.co/api/job/";
-    // var dev = "http://taskcatdev.azurewebsites.net/api/job/";
-    var url =  prod + jobId;
+    angular.forEach(jobId, function (value, key) {
+        console.log(key + " : " + value) 
+        var prod = "http://fetchprod.gobd.co/api/job/";
+        // var dev = "http://taskcatdev.azurewebsites.net/api/job/";
+        var url =  prod + value;
 
-    $http({
-        method: 'GET',
-        url: url,                
-    }).then(function success(response){
-        vm.loadingJob = false;
-        vm.job = response.data;
-        //// FIXME: unveil this piece of code when bulk order creation is done
-        $( function() {
-            $( "#dialog-confirm" ).dialog({
-              resizable: false,
-              height: "auto",
-              width: 400,
-              modal: true,
-              buttons: {
-                "Reciever Will Pay": function() {
-                  $( this ).dialog( "close" );
-                },
-                "Vendor Will Pay": function() {
-                    
-                    if (vm.job.Order.OrderCart.TotalToPay !== "") {
-                        vm.job.Order.OrderCart.TotalToPay -= vm.job.Order.OrderCart.ServiceCharge;                        
+        $http({
+            method: 'GET',
+            url: url,
+        }).then(function success(response){
+            vm.loadingJob = false;
+            var job = response.data;            
+            if (job.User.UserName === "B2C") {
+                if (job.Order.OrderCart.SubTotal === 0) {
+                    job.Order.OrderCart.SubTotal = "";
+                    job.Order.OrderCart.TotalToPay = "";
+                }
+
+                for (var i = job.Order.OrderCart.PackageList.length - 1; i >= 0; i--) {
+                    if (job.Order.OrderCart.PackageList[i].Price === 0) {
+                        job.Order.OrderCart.PackageList[i].Price = "";
+                        job.Order.OrderCart.TotalToPay = "";                                                    
                     }
-                    vm.job.Order.OrderCart.ServiceCharge = 0;
-                    vm.$apply();
-                    $( this ).dialog( "close" );
+                    if (job.Order.OrderCart.PackageList[i].Weight === 0) {
+                        job.Order.OrderCart.PackageList[i].Weight = "";
+                    }
+                    if (job.Order.OrderCart.PackageList[i].Total === 0) {
+                        job.Order.OrderCart.PackageList[i].Total = "";
+                    }
+                    
                 }
-              }
-            });
-        } );
-
-        // FIXME: if user is B2C, then we might not know product price in advance
-        // hence keeping a blank space on the invoice to write product price later
-        // by hand
-        if (vm.job.User.UserName === "B2C") {
-            if (vm.job.Order.OrderCart.SubTotal === 0) {
-                vm.job.Order.OrderCart.SubTotal = "";
-                vm.job.Order.OrderCart.TotalToPay = "";
             }
-
-            for (var i = vm.job.Order.OrderCart.PackageList.length - 1; i >= 0; i--) {
-                if (vm.job.Order.OrderCart.PackageList[i].Price === 0) {
-                    vm.job.Order.OrderCart.PackageList[i].Price = "";
-                    vm.job.Order.OrderCart.TotalToPay = "";                                                    
-                }
-                if (vm.job.Order.OrderCart.PackageList[i].Weight === 0) {
-                    vm.job.Order.OrderCart.PackageList[i].Weight = "";
-                }
-                if (vm.job.Order.OrderCart.PackageList[i].Total === 0) {
-                    vm.job.Order.OrderCart.PackageList[i].Total = "";
-                }
-                
-            }
-
-        }        
-    }, function error(error) {
-        alert(error.Message)
+            vm.jobs.push(job);
+            console.log(job);            
+        }, function error(error) {
+            alert(error.Message)
+        })      
     })
+
+    
 };
