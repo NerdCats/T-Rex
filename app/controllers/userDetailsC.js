1b98e030f075d4c1047e23a67afa1e61f5bba84f
tareq89 <tareqaziz.aust.cse@gmail.com>
2016-11-27 11:47:01 +0600

    GFETCH-270 added delivery, pickup area and date range filter support

--- app/controllers/userDetailsC.js
+++ app/controllers/userDetailsC.js
@@ -1,17 +1,23 @@
 'use strict';
-app.controller('userDetailsC', ['$scope', '$routeParams', 'ngAuthSettings', 'restCall', 'dashboardFactory', userDetailsC]);
+app.controller('userDetailsC', ['$scope', '$routeParams', 'ngAuthSettings', 'restCall', 'dashboardFactory', 'Areas', userDetailsC]);
 
-function userDetailsC($scope, $routeParams, ngAuthSettings, restCall, dashboardFactory){
+function userDetailsC($scope, $routeParams, ngAuthSettings, restCall, dashboardFactory, Areas){
 	
 	var vm = $scope;
 	vm.userId = $routeParams.id;
 	vm.User = {};	
 	vm.isLoadingUser = true;
+	vm.EnterpriseUsers = [];
+	vm.Areas = Areas;
+	vm.DeliveryArea = null;
+	vm.PickupArea = null;
 	
 	vm.allOrders = dashboardFactory.orders(null);
 	vm.pendingOrders = dashboardFactory.orders("ENQUEUED");
 	vm.inProgressOrders = dashboardFactory.orders("IN_PROGRESS");
-	vm.assignedOrders = dashboardFactory.orders(null)
+	vm.assignedOrders = dashboardFactory.orders(null);
+
+	dashboardFactory.getUserNameList("ENTERPRISE", vm.EnterpriseUsers);
 
 	var userUrl = ngAuthSettings.apiServiceBaseUri + "api/account/profile/" + vm.userId;
 
@@ -29,38 +35,78 @@ function userDetailsC($scope, $routeParams, ngAuthSettings, restCall, dashboardF
 	}
 	restCall('GET', userUrl, null, userFound, userNotFound);
 
+	vm.setDate = function () {
+		var startDateISO = undefined;
+		var endDateISO = undefined;
+		
+		if (vm.startDate&&vm.endDate) {
+			startDateISO = dashboardFactory.getIsoDate(vm.startDate,0,0,0);			
+			endDateISO = dashboardFactory.getIsoDate(vm.endDate,23,59,59);			
+		}
+
+		vm.pendingOrders.searchParam.CreateTime.startDate = startDateISO;
+		vm.pendingOrders.searchParam.CreateTime.endDate = endDateISO;
+
+		// vm.inProgressOrders.searchParam.CreateTime.startDate = startDateISO;
+		// vm.inProgressOrders.searchParam.CreateTime.endDate = endDateISO;
+	}
+
+	vm.clearDate = function () {
+		vm.startDate = undefined;
+		vm.endDate = undefined;
+
+		vm.activate();
+	}
+
 	vm.loadUserOrEnterpriseJobs = function () {		
 		vm.allOrders.isCompleted = 'IN_PROGRESS';
 		vm.allOrders.searchParam.UserName = vm.User.UserName;
 		vm.allOrders.searchParam.PageSize = 50;		
+		vm.allOrders.searchParam.DeliveryArea = vm.DeliveryArea;
+		vm.allOrders.searchParam.PickupArea = vm.PickupArea;
 		vm.allOrders.loadOrders();
 	}
 
 	vm.load_Pending_Or_Inprogress_Jobs_For_Asset_To_Assign = function () {
-				
+
+		vm.setDate();
+
+		vm.allOrders.searchParam.UserName = vm.EnterpriseUser;
+		vm.pendingOrders.searchParam.UserName = vm.EnterpriseUser;
+		vm.inProgressOrders.searchParam.UserName = vm.EnterpriseUser;
+
+
+		vm.allOrders.searchParam.DeliveryArea = vm.DeliveryArea;
+		vm.pendingOrders.searchParam.DeliveryArea = vm.DeliveryArea;
+		vm.inProgressOrders.searchParam.DeliveryArea = vm.DeliveryArea;
+
+		vm.allOrders.searchParam.PickupArea = vm.PickupArea;
+		vm.pendingOrders.searchParam.PickupArea = vm.PickupArea;
+		vm.inProgressOrders.searchParam.PickupArea = vm.PickupArea;
+
 		vm.pendingOrders.isCompleted = 'ENQUEUED';		
 		vm.pendingOrders.searchParam.PageSize = 50;
 
+		vm.pendingOrders.isCompleted = 'IN_PROGRESS';
 		vm.pendingOrders.assign.showPickupAssign = true;
 		vm.pendingOrders.assign.showdeliveryAssign = true;
 		vm.pendingOrders.assign.showsecuredeliveryAssign = true;
 		vm.pendingOrders.assign.assetRef = vm.userId;
 		vm.pendingOrders.loadOrders();
 		
-		vm.inProgressOrders.isCompleted = 'IN_PROGRESS';		
+		vm.inProgressOrders.isCompleted = 'IN_PROGRESS';
 		vm.pendingOrders.searchParam.PageSize = 50;
 		vm.inProgressOrders.assign.showPickupAssign = true;
 		vm.inProgressOrders.assign.showdeliveryAssign = true;
 		vm.inProgressOrders.assign.showsecuredeliveryAssign = true;
 		vm.inProgressOrders.assign.assetRef = vm.userId;
-		vm.inProgressOrders.loadOrders();
-
-		
+		vm.inProgressOrders.loadOrders();	
 
 		vm.assignedOrders.searchParam.userId = vm.userId;
 		vm.assignedOrders.searchParam.PageSize = 50;
 		vm.assignedOrders.loadOrders();
 	}
+	
 
 	vm.activate = function () {
 		if (vm.User.Type === "USER" || vm.User.Type === "ENTERPRISE") {
