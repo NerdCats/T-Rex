3f3da932c40ce2a6328c7538fb2bbfeaa0514bf4
tareq89 <tareqaziz.aust.cse@gmail.com>
2016-12-22 17:11:41 +0600

    GFETCH-315 adding by default list of jobs in bulk assign

--- app/controllers/bulkAssignC.js
+++ app/controllers/bulkAssignC.js
@@ -1,13 +1,65 @@
 app.controller('bulkAssignC', bulkAssignC);
 
-function bulkAssignC($scope, dashboardFactory){
+function bulkAssignC($scope, $http, ngAuthSettings, dashboardFactory){
 	var vm = $scope;
 	vm.listOfHRID = [];
+	vm.Assets = [];
+	vm.selectedAssetId = null;
+	vm.EnterpriseUser = null;
+	vm.PaymentStatus = null;	
+	vm.DeliveryArea = null;
+	vm.searchKey = null;
 	vm.Orders = dashboardFactory.orders(null);	
+
+	vm.Orders.assign.showPickupAssign = true;
+	vm.Orders.assign.showdeliveryAssign = true;
+	vm.Orders.assign.showsecuredeliveryAssign = true;
+	vm.Orders.searchParam.jobState === 'IN_PROGRESS';
+
+
+	vm.assetChanged = function () {		
+		vm.Orders.assign.assetRef = vm.selectedAssetId;
+	}
+	vm.getAssetsList = function (page) {		
+		var getUsersUrl = ngAuthSettings.apiServiceBaseUri + "api/Account/odata?$filter=Type eq 'BIKE_MESSENGER'&$orderby=UserName&page="+ page +"&pageSize=50";
+		dashboardFactory.getUserNameList(getUsersUrl).then(function (response) {
+			if (page === 0) {
+				vm.Assets = [];
+				vm.Assets.push(null);
+			}
+			angular.forEach(response.data, function (value, index) {
+				vm.Assets.push(value);
+			})
+			if (response.pagination.TotalPages > page) {
+				vm.getAssetsList(page + 1);
+			}			
+		}, function (error) {
+			console.log(error);
+		});
+	};
+
+	vm.getAssetsList();
+
+	vm.activate = function () {
+		vm.Orders.searchParam.UserName = vm.EnterpriseUser;
+		vm.Orders.searchParam.PaymentStatus = vm.PaymentStatus;
+		vm.Orders.searchParam.pageSize = vm.jobPerPage;
+		vm.Orders.searchParam.DeliveryArea = vm.DeliveryArea;
+		vm.Orders.searchParam.subStringOf.SearchKey = vm.searchKey;
+		vm.Orders.searchParam.orderby.property = "ModifiedTime";
+		vm.Orders.isCompleted = 'IN_PROGRESS';			
+		vm.Orders.loadOrders();
+	}
+
 	vm.$watch(function () {
 		return vm.listOfHRID;
 	}, function (newVal, oldVal) {
+		if (newVal.length === 0) {
+			vm.activate();
+		}
 		if (newVal != oldVal) {
+			vm.Orders.pagination = null;
+			vm.Orders.pages = null;
 			vm.Orders.loadListOfOrders(newVal);
 		}
 	})
