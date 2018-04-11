c4913917de6a29f4338e0ba8e0321d7292986daa
ririyad <ririyad.291@gmail.com>
2018-04-11 06:28:56 +0600

    address info added

--- app/apiServiceUri/apiServiceBaseUri.js
+++ app/apiServiceUri/apiServiceBaseUri.js
@@ -1,6 +1,6 @@
 app.constant('ngAuthSettings', {	
-	apiServiceBaseUri: "http://fetchdev.gobd.co/",
+	// apiServiceBaseUri: "http://fetchdev.gobd.co/",
 	// apiServiceBaseUri: "http://localhost:8160/",
-	// apiServiceBaseUri: "http://localhost:8177/",
+	apiServiceBaseUri: "http://localhost:8177/",
 	clientId: 'GoFetchDevWebApp'
 });
